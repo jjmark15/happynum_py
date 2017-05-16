08467013a77dbf6a5eab83b2bd1cfa9d9b226780
jjmark15 <jjmark15@gmail.com>
2017-05-16 23:21:01 +0100

    print in columns, sorting method improved

--- find_earliest_indicator.py
+++ find_earliest_indicator.py
@@ -1,5 +1,3 @@
-import operator
-
 
 def square_sum(n):
     ss = 0
@@ -24,17 +22,18 @@ def ishappy(n):
 
 
 dups = {4: 0, 16: 0, 20: 0, 37: 0, 42: 0, 58: 0, 89: 0, 145: 0}
+test = []
 r = 100 # number range
 for n in range(1, r + 1):
     new = ishappy(n)
     for dup in new.keys():
         if dups[dup] != 0:
-            dups[dup] += new[dup]/r
+            dups[dup] += new[dup]/r # where r is the number range
         else:
             dups[dup] = new[dup]/r
 
 # list that has dups in sorted order
-sorted_dups = sorted(dups.items(), key=operator.itemgetter(1))
+sorted_dups = sorted(dups.items(), key=lambda x:x[1])
 
 print('For a number range of {n}:\n'.format(n=r))
 print('Indicator vs average iterations until hit:')
@@ -42,5 +41,5 @@ min = min(dups.values())
 for dup, c in sorted_dups:
     if dups[dup] == min:
         min = dup
-    print(dup, round(c, 1))
+    print("n = {0:3} : c = {1:2}".format ( dup, round(c, 1) ))
 input('\nEarliest indicator is {v}\n'.format(v=min))
