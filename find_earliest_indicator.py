510b159203fb6799ca5ac6464a68470042a82abc
jjmark15 <jjmark15@users.noreply.github.com>
2017-09-24 21:53:03 +0100

    neaten formatting

--- find_earliest_indicator.py
+++ find_earliest_indicator.py
@@ -1,4 +1,3 @@
-
 def square_sum(n):
     ss = 0
     for digit in str(n):
@@ -12,7 +11,7 @@ def ishappy(n):
     dups = {4: 0, 16: 0, 20: 0, 37: 0, 42: 0, 58: 0, 89: 0, 145: 0}
     while 0 in dups.values():
         ss = square_sum(ss)
-        if ss == 1: # therefore happy
+        if ss == 1:  # therefore happy
             return {}
             break
         if ss in dups.keys() and dups[ss] == 0:
@@ -22,17 +21,17 @@ def ishappy(n):
 
 
 dups = {4: 0, 16: 0, 20: 0, 37: 0, 42: 0, 58: 0, 89: 0, 145: 0}
-r = 100 # number range
+r = 100  # number range
 for n in range(1, r + 1):
     new = ishappy(n)
     for dup in new.keys():
         if dups[dup] != 0:
-            dups[dup] += new[dup]/r # where r is the number range
+            dups[dup] += new[dup] / r  # where r is the number range
         else:
-            dups[dup] = new[dup]/r
+            dups[dup] = new[dup] / r
 
 # list that has dups in sorted order
-sorted_dups = sorted(dups.items(), key=lambda x:x[1])
+sorted_dups = sorted(dups.items(), key=lambda x: x[1])
 
 print('For a number range of {n}:\n'.format(n=r))
 print('Indicator vs average iterations until hit:')
@@ -40,5 +39,5 @@ min = min(dups.values())
 for dup, c in sorted_dups:
     if dups[dup] == min:
         min = dup
-    print("n = {0:3} : c = {1:2}".format ( dup, round(c, 1) ))
+    print("n = {0:3} : c = {1:2}".format(dup, round(c, 1)))
 input('\nEarliest indicator is {v}\n'.format(v=min))
