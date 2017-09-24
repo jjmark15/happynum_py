1e2fbffffbd3410125034c6d5d74af6907cdc9a6
jjmark15 <jjmark15@users.noreply.github.com>
2017-09-24 21:48:34 +0100

    improve method

--- dist_happy_num_range_calc.py
+++ dist_happy_num_range_calc.py
@@ -2,6 +2,14 @@ from time import clock
 from mypackage.mymodule import num_input
 
 
+def is_first_it(n):
+    ascending = int("".join(sorted(str(n))))
+    if n == ascending:
+        return True
+    else:
+        return False
+
+
 def square_sum(n):
     ss = 0
     for digit in str(n):
@@ -13,46 +21,33 @@ def ishappy(n):
     ss = n
     while True:
         ss = square_sum(ss)
-        # if ss == 89 or ss == 0: # either of these indicates not happy
-        if ss in [89, 145, 42, 37, 58, 20, 4, 16]: # this turns out to be faster
+        if ss in [89, 145, 42, 37, 58, 20, 4, 16]:
             return False
-            break
-        elif ss == 1: # therefore happy
+        elif ss == 1:  # therefore happy
             return True
-            break
 
 
 def get_dist_happy(r):
-    happy_numbers = []
     count = 0
-
-    # cycle through range
-    for i in range(1, r + 1):
-        # sort number digit order
-        i = int(''.join(sorted([d for d in str(i)])))
-        if i not in happy_numbers: # check is distinct
-            if ishappy(i):
-                happy_numbers.append(i)
-                count += 1
-    return count, happy_numbers
+    range_gen = (i for i in range(1, r) if is_first_it(i)
+        )  # save memory with generator
+    for i in range_gen:
+        if ishappy(i):
+            count += 1
+    return count
 
 
 def main():
     print('Distinct Happy Number Range Counter\n')
-    # define range
-    r = num_input('Number Range: ')
-    time_start = clock() # start timer
-    count, nums = get_dist_happy(r)
+    r = num_input('Number Range: ')  # define range
+    time_start = clock()  # start timer
+    count = get_dist_happy(r)
 
-    time_end = clock() # end timer
+    time_end = clock()  # end timer
     time_delta = time_end - time_start
     print('Count Total: {count}'.format(count=count))
-    print('Calc Time: {} s'.format(round(time_delta,5)))
-    p = input('See number list? (y): ')
-    if p == 'y':
-        for n in nums:
-            print(n)
-        input()
+    print('Calc Time: {}s'.format(round(time_delta, 5)))
+    input()
 
 
 if __name__ == "__main__":
