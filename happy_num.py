f59f9d3d7b8f78031f572ba72844c6bc74bb4a70
jjmark15 <jjmark15@users.noreply.github.com>
2017-10-04 21:17:19 +0100

    aesthetic formatting

--- happy_num.py
+++ happy_num.py
@@ -29,8 +29,8 @@ def ishappy(n):
 
 def get_dist_happy(r):
     count = 0
-    range_gen = (i for i in range(1, r + 1) if is_first_it(i)
-        )  # save memory with generator
+    range_gen = (i for i in range(1, r + 1)
+                 if is_first_it(i))  # save memory with generator
     for i in range_gen:
         if ishappy(i):
             count += 1
@@ -40,8 +40,8 @@ def get_dist_happy(r):
 def main():
     # set up argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', type=int, default=1E06,
-                        help='Range to be calculated?')
+    parser.add_argument(
+        '-r', type=int, default=1E06, help='Range to be calculated?')
     args = parser.parse_args()
 
     # start main script
