ddb9264cb988b68bf61822906bc3a2602566030c
Josh Jones <jjmark15@users.noreply.github.com>
2018-05-13 16:03:39 +0100

    move to vscode and separate app from logic

--- happy_num.py
+++ happy_num.py
@@ -1,7 +1,3 @@
-from time import clock
-import argparse
-
-
 def is_first_it(n):
     ascending = int("".join(sorted(str(n))))
     if n == ascending:
@@ -37,45 +33,3 @@ def get_dist_happy(r, verbose=False):
                 print(i)
             count += 1
     return count
-
-
-def get_args():
-    # set up argument parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-r', type=float, default=1E06, help='Range to be calculated?')
-    parser.add_argument(
-        '-v', default=False, action='store_true', help="Verbose?"
-    )
-    args = parser.parse_args()
-    args.r = int(args.r)
-    return args
-
-
-def format_dec(n):
-    a, b = ('%E' % n).split('E')
-    out = str(round(float(a.rstrip('0').rstrip('.')), 3))
-    if b != '+00':
-        out += 'e' + b
-    return out
-
-
-def main():
-    args = get_args()
-
-    # start main script
-    print('Distinct Happy Number Range Counter\n')
-    print('Range:', args.r)
-    time_start = clock()  # start timer
-
-    count = get_dist_happy(args.r, args.v)
-
-    time_end = clock()  # end timer
-    time_delta = time_end - time_start
-    print('Count Total: {count}'.format(count=count))
-    print('Calc Time: {}s'.format(format_dec(time_delta)))
-
-
-if __name__ == "__main__":
-    main()
-    input('\nEnter to close...')
