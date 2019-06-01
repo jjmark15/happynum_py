32a4ee5595a1c5c0eb06e7f6cd9e27afa930895d
Josh Jones <ohblonddev@gmail.com>
2019-06-01 14:23:10 +0100

    Improve performance (#3)
    * use acii values for checking if sorted in is_first_iteration
    
    * use modulo method to iterate through digits in number
    * state typing for functions in happy_num
    
    * use yapf formatter
    *r equire python 3.7*

--- happy_num.py
+++ happy_num.py
@@ -1,12 +1,28 @@
-def is_first_iteration(n):
-    return n == int("".join(sorted(str(n))))
+from math import pow
 
 
-def square_sum(n):
-    return sum([int(d)**2 for d in str(n)])
+def is_first_iteration(n: int):
+    prev: str = "0"
+
+    for (i, curr) in enumerate(str(n)):
+        if i > 0:
+            if prev > curr:
+                return False
+        prev = curr
+    return True
 
 
-def ishappy(n):
+def square_sum(n: int):
+    ss = 0
+    val = 0 + n
+
+    while val > 0:
+        ss += pow((val % 10), 2)
+        val: int = int(val/10)
+    return ss
+
+
+def ishappy(n: int):
     unhappy_markers = [89, 145, 42, 37, 58, 20, 4, 16]
     ss = n
     while True:
@@ -17,7 +33,7 @@ def ishappy(n):
             return True
 
 
-def get_dist_happy(r, verbose=False):
+def get_dist_happy(r: int, verbose: bool = False):
     count = 0
     range_gen = (i for i in range(1, r + 1)
                  if is_first_iteration(i))  # save memory with generator
