1e4d824254595bd6e274d0886281e01d69b19ee3
Josh Jones <ohblonddev@gmail.com>
2019-06-01 14:18:27 +0100

    use modulo method to iterate through digits in number state typing for functions in happy_num

--- happy_num.py
+++ happy_num.py
@@ -1,5 +1,8 @@
-def is_first_iteration(n):
-    prev = "0"
+from math import pow
+
+
+def is_first_iteration(n: int):
+    prev: str = "0"
 
     for (i, curr) in enumerate(str(n)):
         if i > 0:
@@ -9,11 +12,17 @@ def is_first_iteration(n):
     return True
 
 
-def square_sum(n):
-    return sum([int(d)**2 for d in str(n)])
+def square_sum(n: int):
+    ss = 0
+    val = 0 + n
+
+    while val > 0:
+        ss += pow((val % 10), 2)
+        val: int = int(val/10)
+    return ss
 
 
-def ishappy(n):
+def ishappy(n: int):
     unhappy_markers = [89, 145, 42, 37, 58, 20, 4, 16]
     ss = n
     while True:
@@ -24,7 +33,7 @@ def ishappy(n):
             return True
 
 
-def get_dist_happy(r, verbose=False):
+def get_dist_happy(r: int, verbose: bool = False):
     count = 0
     range_gen = (i for i in range(1, r + 1)
                  if is_first_iteration(i))  # save memory with generator
